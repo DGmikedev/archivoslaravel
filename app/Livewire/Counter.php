bf87d3b1d2d3123cf63eaf39982eb2cc330b6ffd
DGmike.dev <dgmike.dev@gmail.com>
2025-06-03 19:09:57 -0600

    testlivewire

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -4,23 +4,18 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
 class Counter extends Component
 {
 
-    public $count = 0;
-    public $saludo = "Hola mundo";
-    public $categories = [1994,2000,2005,2010,2015,2020,2023];
-
-    public function increment(){
-        $this->count++;
-    }
-
-    public function actualiza_grafica(){
-        return "hola";
-    }
+    public $msg = "MENSAJE RELACIONADO CON LE BENVENIDA AL USUARIO";
+    public $txt;
+    public $cambio;
 
     public function render()
     {
         return view('livewire.counter');
     }
+
+   
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gestor archvios | Home</title>
-    <script src="https://code.highcharts.com/highcharts.js"></script><!--
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script> 
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script> -->
-    @livewireStyles
 </head>
 <body>
     
-
+    <h1>TABLA DE VALORES 2024</h1>
     <livewire:counter />
 
     @livewireScripts
+
+
 </body>
 </html>
