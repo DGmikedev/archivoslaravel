92b17cff8b600c1d36fa01c98f155d36df8934d7
DGmike.dev <dgmike.dev@gmail.com>
2025-12-30 15:35:26 -0600

    add chngs docs

--- app/Clases/Documentos/Documento.php
+++ app/Clases/Documentos/Documento.php
@@ -24,6 +24,7 @@ class Fecha
 {
     public static function tipoFecha(string $fecha, string $tipo_fecha = 'N',): string
     {
+        $fecha = new date()
         return  "TipoFecha = $tipo_fecha - $fecha";
     }
 }
